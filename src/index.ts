91922c4b359b0b01614ad7c60e5ab83da11b3d14
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-08-20 10:57:33 +0530

    fixed index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,28 @@ let cors = require('cors');
 let mongoose = require('mongoose');
 
 
-
 const app = express();
 
+//allow cross origin requests
 app.use(
     cors({
         Credentials: true,
     }),
 )
 
+//configure app
 app.use(bodyParser.json());
 app.use(CookieParser());
 app.use(Compression());
 
 
-const server = http.createServer(app);
+const server = http.createServer(app); //create server
 
+//define port for server run
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
 
 //path for mongodb
 let mongoURL = 'mongodb+srv://admin:admin@research-buddy.yw0yvdp.mongodb.net/?retryWrites=true&w=majority'
@@ -34,11 +37,10 @@ let mongoURL = 'mongodb+srv://admin:admin@research-buddy.yw0yvdp.mongodb.net/?re
 mongoose.promise = Promise; //overwrite mongoose promise
 mongoose.connect(mongoURL);
 
-
 try{
     //mongoose.connection.on('error',(error:Error) => console.log('MongoDB connection error: ${err}'));
     mongoose.connection.once('open', () => console.log('MongoDB connected!'));
 }   catch (error) {
-    console.log(error);
+    console.log("Error is",error);
 }
     
